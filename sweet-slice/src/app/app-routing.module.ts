26de2e37eed75fd8ef19ece1e4e6515bb4adb03d
Rachael <rachaelvruggink@gmail.com>
2020-02-26 21:36:35 -0500

    updated routing to show components

--- sweet-slice/src/app/app-routing.module.ts
+++ sweet-slice/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PizzaMakerComponent } from './pizza-maker/pizza-maker.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'checkout',
+    component: CheckoutComponent
+  
+  },
+  {
+    path: '**',
+    component: PizzaMakerComponent
+  
+  }
+  ];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
