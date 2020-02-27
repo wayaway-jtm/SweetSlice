c56b4c8f6121f25c83e059020b013881e5a42cb7
john <wayaway.jtm@gmail.com>
2020-02-26 21:28:42 -0500

    added http req funcs

--- ingredientsAPI.js
+++ ingredientsAPI.js
@@ -1,10 +1,11 @@
 import express from "express";
 import ingredientList from "./ingredients"
+import isUndefined from "util";
 
 
 
 const ingredientEndpts = express.Router();
-
+//#region HTML requests
 // Get all ingredients
 ingredientEndpts.get('/all', (req, res) => {
     res.status(200).json(ingredientList);
@@ -12,26 +13,64 @@ ingredientEndpts.get('/all', (req, res) => {
 
 // Get all ingredient ids
 ingredientEndpts.get('/ids', (req, res) => {
+    res.status(200).json(ingredientList.map(i => i.id));
+});
+
+//#region Types
+// Returns all valid types
+ingredientEndpts.get('/types/all', (req, res) => {
+    res.status(200).json(ingredientList.filter(i => isFirstOfType(i.type, ingredientList.indexOf(i))));
+});
+
+ingredientEndpts.get('/types/:typeName/all', (req, res) => {
+    let lCaseType = req.params.toLowerCase();
+    if (isUndefined(ingredientList.find(i => i.type.toLowerCase() === lCaseType))) {
+        res.status(400).json(`Type ${req.params.typeName} not found`);
+    } else {
+        res.status(200).json(ingredientList.filter(i => i.type.toLowerCase() === lCaseType));
+    }
+});
+
+function isFirstOfType(typeName, index) {
+    return ingredientList.find(i => i.type === typeName) === index;
+}
+//#endregion
+//#region Names
+ingredientEndpts.get('/names/all', (req, res) => {
     let resArray = [];
     for (const ingredient of ingredientList) {
-        resArray.push(ingredient.id);
+        resArray.push(ingredient.name);
     }
     res.status(200).json(resArray);
 });
 
-// Get all ingredient types
-ingredientEndpts.get('/types/all', (req, res) => {
-    res.status(200).json(ingredientList.filter((v, i, a) => a.indexOf(v) === i));
+ingredientEndpts.get('/names/:ingrId', (req, res) => {
+    let resArray = [];
+    for (const ingredient of ingredientList) {
+        resArray.push(ingredient.name);
+    }
+    res.status(200).json(resArray);
+});
+//#endregion
+//#region Individual Ingredient
+ingredientEndpts.get('/:id/name', (req, res) => {
+
 });
 
-ingredientEndpts.get('/', (req, res) => {
-    
+ingredientEndpts.get('/:id/type', (req, res) => {
+
 });
 
-ingredientEndpts.get('/', (req, res) => {
-    
-});
+ingredientEndpts.get('/:id/cost', (req, res) => {
 
-ingredientEndpts.get('/', (req, res) => {
-    
 });
+//#endregion
+//#region Cost
+ingredientEndpts.get('/costs/all', (req, res) => {
+
+});
+ingredientEndpts.get('/costs/:num', (req, res) => {
+
+});
+//#endregion
+//#endregion
