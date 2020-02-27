76ed52ca9a7a5cc5923a1605b6e72ee5d8f6f4fe
Rachael <rachaelvruggink@gmail.com>
2020-02-26 22:00:30 -0500

    html updates

--- sweet-slice/src/app/pizza-maker/pizza-maker.component.ts
+++ sweet-slice/src/app/pizza-maker/pizza-maker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { SERVICEPLACEHOLDER } from '_____/______';
 
 @Component({
   selector: 'app-pizza-maker',
@@ -13,3 +14,17 @@ export class PizzaMakerComponent implements OnInit {
   }
 
 }
+//initialize the array getting the items from the service  
+
+// Setting pizza service to pizza array to ngfor through
+
+// export class PuppyListComponent implements OnInit {
+//   puppies : IPuppy[];
+//   constructor(public service : PuppyFinderService) { }
+
+
+//   ngOnInit(): void {
+//     this.puppies = this.service.getPuppy();
+//   }
+
+// }
