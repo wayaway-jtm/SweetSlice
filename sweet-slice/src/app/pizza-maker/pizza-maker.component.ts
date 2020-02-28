ac0201db5ee5639c5e005df012786d19bc78ca7e
john <wayaway.jtm@gmail.com>
2020-02-27 21:57:52 -0500

    Implemented proper compiling and connecting Angular to Express

--- sweet-slice/src/app/pizza-maker/pizza-maker.component.ts
+++ sweet-slice/src/app/pizza-maker/pizza-maker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { myServer } from '../my-server.service';
 
 @Component({
   selector: 'app-pizza-maker',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaMakerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private server: myServer) { }
 
   ngOnInit(): void {
+    this.server.getAllIngredients();
   }
 
 }
