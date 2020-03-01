2195b2a4d75a4b1b2d95e7d4c0d5c42d17bb0dfc
john <wayaway.jtm@gmail.com>
2020-03-01 16:28:29 -0500

    init forms work

--- sweet-slice/src/app/pizza-maker/pizza-maker.component.ts
+++ sweet-slice/src/app/pizza-maker/pizza-maker.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { myServer } from '../my-server.service';
 import { IIngredient, IPizza } from '../interfaces';
-import { Ingredient, Pizza } from '../classes'
+import { Ingredient, Pizza } from '../classes';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-pizza-maker',
@@ -14,6 +15,11 @@ export class PizzaMakerComponent implements OnInit {
   _crusts: IIngredient[] = [];
   _types: string[] = [];
   _customPizza: Pizza = new Pizza();
+  _pizzaForm: FormGroup = new FormGroup({
+    toppings: new FormGroup({}),
+    frostings: new FormGroup({}),
+    crusts: new FormGroup({}),
+  });
   constructor(private server: myServer) { }
 
   ngOnInit(): void {
@@ -23,6 +29,16 @@ export class PizzaMakerComponent implements OnInit {
     this.server.getAllOfType('topping').subscribe((data: IIngredient[]) => this._toppings = data);
     this.server.getAllOfType('frosting').subscribe((data: IIngredient[]) => this._frostings = data);
     this.server.getAllOfType('crust').subscribe((data: IIngredient[]) => this._crusts = data);
+    // Creating FormControls for each ingredient
+    for (const topping of this._toppings) {
+      this._pizzaForm.addControl(topping.name, new FormControl(topping.name));
+    }
+    for (const frosting of this._frostings) {
+      this._pizzaForm.addControl(frosting.name, new FormControl(frosting.name));
+    }
+    for (const crust of this._crusts) {
+      this._pizzaForm.addControl(crust.name, new FormControl(crust.name));
+    }
   }
 
 }
