6b343d63c14685110cdc4ac6b775ef96b271caf8
john <wayaway.jtm@gmail.com>
2020-03-01 15:38:14 -0500

    Began implementing HTML

--- sweet-slice/src/app/pizza-maker/pizza-maker.component.ts
+++ sweet-slice/src/app/pizza-maker/pizza-maker.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { myServer } from '../my-server.service';
+import { IIngredient, IPizza } from '../interfaces';
+import { Ingredient, Pizza } from '../classes'
 
 @Component({
   selector: 'app-pizza-maker',
@@ -7,11 +9,20 @@ import { myServer } from '../my-server.service';
   styleUrls: ['./pizza-maker.component.css']
 })
 export class PizzaMakerComponent implements OnInit {
-
+  _toppings: IIngredient[] = [];
+  _frostings: IIngredient[] = [];
+  _crusts: IIngredient[] = [];
+  _types: string[] = [];
+  _customPizza: Pizza = new Pizza();
   constructor(private server: myServer) { }
 
   ngOnInit(): void {
-    this.server.getAllIngredients();
+    // Getting all the different type names
+    this.server.getAllTypes().subscribe((data: string[]) => this._types = data);
+    // Getting ingredients by each type so filtering isn't needed later
+    this.server.getAllOfType('topping').subscribe((data: IIngredient[]) => this._toppings = data);
+    this.server.getAllOfType('frosting').subscribe((data: IIngredient[]) => this._frostings = data);
+    this.server.getAllOfType('crust').subscribe((data: IIngredient[]) => this._crusts = data);
   }
 
 }
