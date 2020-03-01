0de83a6968471778a36913202a07a1cd1e97f338
john <wayaway.jtm@gmail.com>
2020-03-01 18:40:35 -0500

    Removed spread operator on toppings in favor of just passing in an array

--- sweet-slice/src/app/my-cart.service.ts
+++ sweet-slice/src/app/my-cart.service.ts
@@ -12,7 +12,7 @@ export class MyCartService {
   cart : IPizza[];
 
   //creates a new pizza and adds it to the cart. Takes ingredients in the order crust, frosting, topping, topping, etc. 
-  addPizza (myCrust : IIngredient, myFrosting : IIngredient, ...myToppings : IIngredient[]) {
+  addPizza (myCrust : IIngredient, myFrosting : IIngredient, myToppings : IIngredient[]) {
     this.cart.push(
       {
         crust : myCrust,
